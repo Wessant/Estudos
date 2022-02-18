b2cb7e8afa2b14d2f4492a54e2ca2a101d619968
Wessant <weslleyd870@gmail.com>
2022-02-18 15:59:36 -0300

    main-004v

--- Codes/aluramidi-curso-arquivos-iniciais/main.js
+++ Codes/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,24 +1,35 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play()
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento === null) {
+        console.log('Elemento não encontrado');
+    }
+
+    if(elemento != null) {
+        console.log(elemento.localName === 'audio');
+        elemento.play();
+    }
 }
 
 const listaDeTeclas =document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while(contador < listaDeTeclas.length) {
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
     
     const tecla = listaDeTeclas[contador]
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
-    
-    //console.log(idAudio);
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-
-    contador++
     
-    //console.log(contador);
+    tecla.onkeydown = function (evento) {
+        if(evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
 }
